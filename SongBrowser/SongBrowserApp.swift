9f378c301da85c4fb16bc7ffc3a31710472dc267
Russell Gordon <rgordon@lcs.on.ca>
2021-02-20 09:49:52 -0500

    Initial Commit

---
+++ SongBrowser/SongBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongBrowserApp.swift
+//  SongBrowser
+//
+//  Created by Russell Gordon on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct SongBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
