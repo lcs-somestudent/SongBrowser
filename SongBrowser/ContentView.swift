b0f965b96974560501c0c2defa86dbdae0f43937
Russell Gordon <rgordon@lcs.on.ca>
2021-02-20 10:04:13 -0500

    Provided a hint to enter an artist name to search for.

--- SongBrowser/ContentView.swift
+++ SongBrowser/ContentView.swift
@@ -19,9 +19,26 @@ struct ContentView: View {
         VStack {
             
             SearchBarView(text: $searchText)
+
+            // Show a prompt when no search text is given
+            if searchText.isEmpty {
+                
+                Spacer()
+                
+                Text("Please enter an artist name")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                
+                Spacer()
+
+            } else {
+                
+                // Push the search bar to the top
+                Spacer()
+
+            }
+
             
-            // Push the search bar to the top
-            Spacer()
             
         }
         
