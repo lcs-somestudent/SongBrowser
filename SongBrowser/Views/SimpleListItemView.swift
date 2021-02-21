875c81db4e57e48b8233aea578bb6bbb28be94ba
Russell Gordon <rgordon@lcs.on.ca>
2021-02-21 17:16:07 -0500

    Added simple list item view

---
+++ SongBrowser/Views/SimpleListItemView.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleListItemView.swift
+//  SongBrowser
+//
+//  Created by Russell Gordon on 2021-02-21.
+//
+
+import SwiftUI
+
+struct SimpleListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SimpleListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        SimpleListItemView()
+    }
+}
